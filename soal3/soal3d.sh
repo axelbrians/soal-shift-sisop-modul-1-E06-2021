6b2a232ad94962493affacc7acb571eee7fd0bb8
axelbrians <axelbrians@gmail.com>
2021-04-02 17:06:47 +0700

    fix banget 3d

--- soal3/soal3d.sh
+++ soal3/soal3d.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+file=$(ls | grep -E "Kelinci_|Kucing_|-|Foto.log|Koleksi_")
 keyPassword=$(date +"%m%d%Y")
-zip -r -P "$keyPassword" Koleksi.zip *-*
-rm -r *-*
+zip -r -P "$keyPassword" Koleksi.zip $file
+rm -r $file
